e80e993caa27fff919c508ac0d67fc477a5e9cea
SergioCarbone <sergio.carbone@gmail.com>
2018-10-13 16:09:38 -0300

    ,

---
+++ RecuperatorioTP/TP2/Entidades/Changuito.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public class Changuito
+    {
+        private List<Producto> productos;
+        private int espacioDisponible;
+        int cantidad;
+
+        public enum ETipo
+        {
+            Dulce, Leche, Snacks, Todos
+        }
+
+        #region "Constructores"
+        private Changuito()
+        {
+            this.productos = new List<Producto>();
+        }
+        public Changuito(int espacioDisponible) : this()
+        {
+            this.espacioDisponible = espacioDisponible;
+            cantidad = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro la concecionaria y TODOS los Productos
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Mostrar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="c">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        public string Mostrar(Changuito c, ETipo tipo) //quitar static
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} disponibles", c.productos.Count, c.cantidad);
+            sb.AppendLine("");
+            foreach (Producto v in c.productos)
+            {
+                switch (tipo)
+                {
+                    case ETipo.Snacks:
+                        if (v is Snacks)
+                        {
+                            sb.AppendLine(v.Mostrar());
+                        }
+                        break;
+                    case ETipo.Dulce:
+                        if (v is Dulce)
+                        {
+                            sb.AppendLine(v.Mostrar());
+                        }
+                        break;
+                    case ETipo.Leche:
+                        if (v is Leche)
+                        {
+                            sb.AppendLine(v.Mostrar());
+                        }
+                        break;
+                    default:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se agregará el elemento</param>
+        /// <param name="p">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Changuito operator +(Changuito c, Producto p)
+        {
+            if (c.espacioDisponible > 0)
+            {
+                foreach (Producto v in c.productos)
+                {
+                    if (v == p)
+                        return c;
+                }
+                c.productos.Add(p);
+                c.espacioDisponible--;
+            }
+            return c;
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se quitará el elemento</param>
+        /// <param name="p">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Changuito operator -(Changuito c, Producto p)
+        {
+            foreach (Producto v in c.productos)
+            {
+                if (v == p && c.espacioDisponible < c.cantidad)
+                {
+                    c.productos.Remove(p);
+                    c.espacioDisponible++;
+                    break;
+                }
+            }
+
+            return c;
+        }
+        #endregion
+    }
+}
