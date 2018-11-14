ca601a917524408cacc678953beb453f276f61f4
SergioCarbone <sergio.carbone@gmail.com>
2018-11-13 21:17:17 -0300

    2018-11-13

---
+++ TP-4/EntidadesHechas/Paquete.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesHechas
+{
+    public delegate void DelegadoEstado(object sender, EventArgs e);
+    public class Paquete : IMostrar<Paquete>
+    {
+        public event DelegadoEstado InformarEstado;
+        public enum EEstado
+        {
+            Ingresado, EnViaje, Entregado
+        }
+
+        private string direccionEntrega;
+        private string trackingID;
+        private EEstado estado;
+
+        public Paquete(string direccionEntrega, string trackingID)
+        {
+            this.DireccionEntrega = direccionEntrega;
+            this.TrackingID = trackingID;
+        }
+
+        public string DireccionEntrega
+        {
+            get
+            {
+                return this.direccionEntrega;
+            }
+            set
+            {
+                this.direccionEntrega = value;
+            }
+        }
+
+        public string TrackingID
+        {
+            get
+            {
+                return this.trackingID;
+            }
+            set
+            {
+                this.trackingID = value;
+            }
+        }
+
+        public EEstado Estado
+        {
+            get
+            {
+                return this.estado;
+            }
+            set
+            {
+                this.estado = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.MostrarDatos(this);
+        }
+
+        /// <summary>
+        /// Dos paquetes serán el mismo si su tracking ID es igual.
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Paquete p1, Paquete p2)
+        {
+            return (p1.trackingID == p2.trackingID);
+        }
+
+        /// <summary>
+        /// Dos paquetes serán distintos si su tracking ID es distinto.
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Paquete p1, Paquete p2)
+        {
+            return !(p1 == p2);
+        }
+
+        #region Alumno
+
+        /// <summary>
+        /// MostrarDatos utilizará string.Format con el siguiente formato "{0} para {1}", p.trackingID, p.direccionEntrega
+        /// </summary>
+        /// <param name="elemento"></param>
+        /// <returns></returns>
+        /// 
+
+        public string MostrarDatos(IMostrar<Paquete> elementos)
+        {
+            List<Paquete> l = (List<Paquete>)((Correo)elementos).Paquetes;
+            string datos = "";            
+            foreach (Paquete p in l)
+            {
+                datos = string.Format("{0} para {1}", p.TrackingID, p.DireccionEntrega);
+            }
+            // Su código
+            return datos;
+        }
+
+        //public string MostrarDatos(IMostrar<List<Paquete>> elementos)
+        //{
+        //    List<Paquete> l = (List<Paquete>)((Correo)elementos).Paquetes;
+        //    string datos = "";
+        //    foreach (Paquete p in l)
+        //    {
+        //        datos = string.Format("{0} para {1}", p.TrackingID, p.DireccionEntrega);
+        //    }
+        //    // Su código            
+        //    return datos;
+        //}
+
+        /// <summary>
+        /// a.	Colocar una demora de 10 segundos.
+        /// b.	Pasar al siguiente estado.
+        /// c.	Informar el estado a través de InformarEstado. EventArgs no tendrá ningún dato extra.
+        /// d.	Repetir las acciones desde el punto A hasta que el estado sea Entregado.
+        /// e.	Finalmente guardar los datos del paquete en la base de datos.
+        /// </summary>
+        public void MockCicloDeVida()
+        {
+            do
+            {
+                System.Threading.Thread.Sleep(10000);
+                this.Estado++;
+                // Informar el estado a través de InformarEstado. EventArgs
+                InformarEstado(this.Estado, null);
+            } while (this.Estado != EEstado.Entregado);
+            PaqueteDAO.Insertar(this);
+        }
+
+        #endregion
+    }
+}
---
+++ TP-4/EntidadesHechas/TrackingIdRepetidoException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesHechas
+{
+    public class TrackingIdRepetidoException : Exception
+    {
+        TrackingIdRepetidoException(string mensaje)
+            : this(mensaje,null)
+        {
+
+        }
+
+        TrackingIdRepetidoException(string mensaje, Exception inner)
+            : base(mensaje,inner)
+        {
+
+        }
+    }
+}
